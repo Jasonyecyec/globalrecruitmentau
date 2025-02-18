04831306ecf3c7168c4af276dce6cb0bb46d1343
Jasonyecyec <jasonyecyec@gmail.com>
2025-02-18 22:53:34 +0800

    First commit

---
+++ app/testpage/component.tsx
@@ -0,0 +1,9 @@
+export default function Component() {
+
+    return (
+        <div>
+            <h1>Component shit</h1>
+        </div>
+    )
+
+}
---
+++ app/testpage/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import  Component  from '@/app/testpage/component';
+
+const TestPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <p>This is a test page.</p>
+
+            <Component/>
+        </div>
+    );
+};
+
+export default TestPage;
