2bbbca6f4638759a00dfa27071182ac49390a02d
Jasonyecyec <jasonyecyec@gmail.com>
2025-03-02 17:26:19 +0800

    fix

--- app/signup/account_success/page.tsx
+++ app/signup/account_success/page.tsx
@@ -27,7 +27,7 @@ import Image from "next/image";
 const AccountSuccess = ()=>{
 
     // const userType = useState<string>('applicant')
-    const userType = "employer"
+    const userType: "employer" | "applicant" = "applicant";
 
     return(
         <div className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
